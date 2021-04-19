baf282f393546606537c87e97a4f9892f998dbb9
John Bannister <eudorajab1@gmail.com>
2021-04-19 19:17:55 +0200

    Updated version

--- __init__.py
+++ __init__.py
@@ -15,6 +15,6 @@ from . import route_manager
 from . import stats
 from . import interceptor_manager
 # optional parameters
-__version__ = "0.0.0"
-__author__ = "you <you@example.com>"
-__license__ = "anything you want"
+__version__ = "1.0.0.0"
+__author__ = "John Bannister <eudorajab1@gmail.com>"
+__license__ = "MIT"
